17378ab64b8bd22067f31aa08e8c12f24400264e
HELLO <bertzorbrist@gmail.com>
2024-01-23 16:56:49 +0300

    init commit

---
+++ src/test/java/SerchWord/SearchWord.java
@@ -0,0 +1,15 @@
+package SerchWord;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchWord {
+
+    @Test
+    void SearchWordGitHub(){
+
+        open();
+    }
+}
